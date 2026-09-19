d18b38ee615d38475928c20e712b967a169a953b
ophielilac <ophielilac@gmail.com>
2026-09-19 23:29:36 +0300

    Bambi-Obeys.user.js

--- Bambi-Obeys.user.js
+++ Bambi-Obeys.user.js
@@ -3,12 +3,12 @@
 // @namespace    BC-Hypnosis
 // @version      1.0.1
 // @description  Bambi Obeys stable loader for Bondage Club
-// @QueenBambi
-// @match https://bondageprojects.elementfx.com/*
-// @match https://www.bondageprojects.elementfx.com/*
-// @match https://bondage-europe.com/*
-// @match https://www.bondage-europe.com/*
-// @grant        GM_addElement
+// @match        https://*.bondageprojects.elementfx.com/R*/*
+// @match        https://*.bondage-europe.com/R*/*
+// @match        https://*.bondageeurope.com/R*/*
+// @match        https://*.bondageprojects.com/R*/*
+// @match        https://*.bondage-asia.com/club/R*
+// @grant        none
 // @run-at       document-end
 // @updateURL    https://raw.githubusercontent.com/ophielilac/Bambi-obeys/main/Bambi-Obeys.user.js
 // @downloadURL  https://raw.githubusercontent.com/ophielilac/Bambi-obeys/main/Bambi-Obeys.user.js
@@ -16,26 +16,13 @@
 
 (function () {
     'use strict';
-
-    // Prevent duplicate loading.
+    
     if (window.__BAMBI_OBEYS_LOADER_RAN__) return;
     window.__BAMBI_OBEYS_LOADER_RAN__ = true;
 
-    // Always request the newest hosted Bambi Obeys core.
-    const coreURL =
-        `https://ophielilac.github.io/Bambi-obeys/Bambi-Obeys.js?${Date.now()}`;
-
-    // GM_addElement is used instead of document.createElement().
-    // This is important for Bondage Club hosts that use CSP restrictions.
-    const script = GM_addElement('script', {
-        src: coreURL,
-        type: 'text/javascript',
-        crossorigin: 'anonymous'
-    });
-
-    if (!script) {
-        console.error(
-            'Bambi Obeys: failed to inject the hosted core.'
-        );
-    }
+    const script = document.createElement('script');
+    script.language = 'JavaScript';
+    script.setAttribute('crossorigin', 'anonymous');
+    script.src = `https://ophielilac.github.io/Bambi-obeys/Bambi-Obeys.js?${Date.now()}`;
+    document.head.appendChild(script);
 })();
