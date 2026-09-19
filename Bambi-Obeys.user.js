5d1a4b5d59916a59be6ccdd674f6edc6f47790e8
ophielilac <ophielilac@gmail.com>
2026-09-19 13:42:33 +0300

    Bambi-Obeys.user.js

--- Bambi-Obeys.user.js
+++ Bambi-Obeys.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Bambi Obeys
 // @namespace    BC-Hypnosis
-// @version      1.0.0
+// @version      1.5.8
 // @description  Bambi Obeys stable loader for Bondage Club
 // @match        https://*.bondageprojects.elementfx.com/R*/*
 // @match        https://*.bondage-europe.com/R*/*
 // @match        https://*.bondageprojects.com/R*/*
-// @match        https://*.bondage-asia.com/club/R*
+// @match        https://*.bondage-asia.com/club/R*/*
 // @grant        none
 // @run-at       document-end
 // @updateURL    https://raw.githubusercontent.com/ophielilac/Bambi-obeys/main/Bambi-Obeys.user.js
@@ -16,14 +16,17 @@
 (function () {
     'use strict';
 
-    // LSCG-style deployment: this tiny loader always pulls the
-    // newest hosted Bambi Obeys core on every page load.
+    // LSCG-style deployment:
+    // This tiny loader always pulls the newest hosted Bambi Obeys core
+    // on every page load.
     if (window.__BAMBI_OBEYS_LOADER_RAN__) return;
     window.__BAMBI_OBEYS_LOADER_RAN__ = true;
 
     const script = document.createElement('script');
     script.language = 'JavaScript';
     script.setAttribute('crossorigin', 'anonymous');
-    script.src = `https://ophielilac.github.io/Bambi-obeys/Bambi-Obeys.js?${Date.now()}`;
+    script.src =
+        `https://ophielilac.github.io/Bambi-obeys/Bambi-Obeys.js?${Date.now()}`;
+
     document.head.appendChild(script);
 })();
